055ededbd1e3c288611276785044a7398492138a
angusjellis@live.com <angusjellis@live.com>
2021-07-20 21:19:56 +0100

    make laptop locking script better

--- beannoying.sh
+++ beannoying.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while :
 do
-	say "it's time to fix your terminal"
+	say "you really should lock your laptop. w w w w w w w w w w w w ww w ww w w w w w w w w w w w w w w w w w w w w w w w w w w "
 done
